8d5d797370d1c08fd57685c9819f329716628873
Abdul Rehman <abduls03357@gmail.com>
2019-11-13 19:46:07 +0500

    first commit

---
+++ app/src/main/java/com/example/hostelworld/HosteliteLAndingPage.java
@@ -0,0 +1,14 @@
+package com.example.hostelworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HosteliteLAndingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hostel_layout);
+    }
+}
