cf889bb48eae7f74d99bebffb95cb3814071cbee
Abdul Rehman <abduls03357@gmail.com>
2019-11-16 00:03:42 +0500

    thirdCommit

---
+++ app/src/main/java/com/example/hostelworld/HostelProfileHostelite.java
@@ -0,0 +1,14 @@
+package com.example.hostelworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HostelProfileHostelite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel_profile_hostelite);
+    }
+}
