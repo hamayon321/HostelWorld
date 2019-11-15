4d63176f5cd34a0885a6ba2ae8d5c4a4cb1f0313
Abdul Rehman <abduls03357@gmail.com>
2019-11-15 08:37:29 +0500

    secondCommit

--- app/src/main/java/com/example/hostelworld/HosteliteLAndingPage.java
+++ app/src/main/java/com/example/hostelworld/HosteliteLAndingPage.java
@@ -1,14 +1,27 @@
 package com.example.hostelworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.hostelworld.adapters.hostelsViewAdapter;
+
 public class HosteliteLAndingPage extends AppCompatActivity {
+
+    RecyclerView rvHostels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hostel_layout);
+        setContentView(R.layout.activity_hostelite_landing_page);
+
+        rvHostels=findViewById(R.id.rv_hostels_lp);
+        rvHostels.setLayoutManager(new LinearLayoutManager(this));
+
+        hostelsViewAdapter adapter =new hostelsViewAdapter(this);
+        rvHostels.setAdapter(adapter);
+
     }
 }
